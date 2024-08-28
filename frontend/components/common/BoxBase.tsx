8e9851259b5da5ac925a7f60c7f4d8375eb1c10f
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-08-28 01:09:57 -0400

    front: updated landing page

--- frontend/components/common/BoxBase.tsx
+++ frontend/components/common/BoxBase.tsx
@@ -1,11 +1,21 @@
+const paddingClasses = {
+  sm: "px-4 py-12",
+  md: "px-6 py-20",
+  lg: "px-8 py-24",
+};
+
 export default function BoxBase({
   children,
+  size = "md",
 }: Readonly<{
   children: React.ReactNode;
+  size?: keyof typeof paddingClasses;
 }>) {
   return (
     <section className="overflow-hidden bg-white py-16 dark:bg-gray-dark md:py-[40px] xl:py-[70px] 2xl:py-[100px] flex justify-center items-center">
-      <section className="px-6 py-20 w-full max-w-[1536px]">{children}</section>
+      <section className={`${paddingClasses[size]} w-full max-w-[1536px]`}>
+        {children}
+      </section>
     </section>
   );
 }
--- frontend/components/landing-page/Hero.tsx
+++ frontend/components/landing-page/Hero.tsx
@@ -1,30 +1,43 @@
 "use client";
 
-import Link from "next/link";
 import { Button } from "@nextui-org/react";
 import BoxBase from "../common/BoxBase";
+import { Image } from "@nextui-org/react";
 
 export default function Hero() {
   return (
     <>
-      <BoxBase>
-        <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4">
-            <div className="mx-auto max-w-[800px] text-center">
-              <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+      <BoxBase size="sm">
+        <div className="mx-auto px-4 md:px-8 2xl:px-0">
+          <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
+            <div className="md:w-1/2">
+              <h1 className="mb-5 text-4xl md:text-4xl lg:text-5xl font-bold leading-tight text-black dark:text-white sm:leading-tight md:leading-tight">
                 Abre las puertas a un mundo de oportunidades con Academy
               </h1>
-              <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+              <p className="mb-5 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
                 Empieza hoy mismo tu viaje hacia el dominio del inglés. Academy
                 es la mejor manera de aprender inglés en línea.
               </p>
-              <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                <Link href="/auth/signin">
-                  <Button color="primary" variant="shadow" size="lg">
-                    Comienza hoy
-                  </Button>
-                </Link>
+              <div className="flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                <Button
+                  className="bg-zinc-900 text-white"
+                  radius="sm"
+                  variant="solid"
+                  size="lg"
+                >
+                  Comienza hoy
+                </Button>
               </div>
+            </div>
+
+            <div className="hidden md:flex md:justify-center md:w-1/2">
+              <Image
+                isBlurred
+                width={240}
+                src="/hero-student.jpg"
+                alt="Estudiante"
+                className="m-5"
+              />
             </div>
           </div>
         </div>
