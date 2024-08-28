763f58daf0f31149104c33c5f5eff92c99e7e0a2
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-08-28 01:12:30 -0400

    front: updated landing page

--- frontend/components/landing-page/Hero.tsx
+++ frontend/components/landing-page/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
         <div className="mx-auto px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className="md:w-1/2">
-              <h1 className="mb-5 text-4xl md:text-4xl lg:text-5xl font-bold leading-tight text-black dark:text-white sm:leading-tight md:leading-tight">
+              <h1 className="mb-5 text-5xl md:text-4xl lg:text-5xl font-bold leading-tight text-black dark:text-white sm:leading-tight md:leading-tight">
                 Abre las puertas a un mundo de oportunidades con Academy
               </h1>
               <p className="mb-5 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
