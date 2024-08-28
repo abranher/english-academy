1c582b3b3ba11817a1cf4b18b0dfabf05264acb8
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-08-28 01:33:49 -0400

    back: installed @nest/config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
