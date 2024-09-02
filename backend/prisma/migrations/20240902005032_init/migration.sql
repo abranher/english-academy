0b8d26df862a9b25a45a1a4f3b7c23fd67b48d81
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-09-01 20:51:33 -0400

    back: created seed

--- backend/prisma/migrations/20240902005032_init/migration.sql
+++ backend/prisma/migrations/20240902005032_init/migration.sql
@@ -40,6 +40,8 @@ CREATE TABLE "Student" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "levelId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
 );
@@ -47,11 +49,13 @@ CREATE TABLE "Student" (
 -- CreateTable
 CREATE TABLE "Tutor" (
     "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
     "biography" TEXT,
     "approvedAt" TIMESTAMP(3),
     "location" TEXT NOT NULL,
     "curriculumUrl" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Tutor_pkey" PRIMARY KEY ("id")
 );
@@ -59,12 +63,12 @@ CREATE TABLE "Tutor" (
 -- CreateTable
 CREATE TABLE "Certification" (
     "id" TEXT NOT NULL,
-    "tutorId" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "certification" TEXT NOT NULL,
     "institution" TEXT NOT NULL,
     "issueDate" TIMESTAMP(3) NOT NULL,
     "language" TEXT NOT NULL,
+    "tutorId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -76,15 +80,27 @@ CREATE TABLE "Country" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "code" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Country_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Skill" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Skill_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Level" (
     "id" TEXT NOT NULL,
     "levelCode" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -95,11 +111,13 @@ CREATE TABLE "Level" (
 -- CreateTable
 CREATE TABLE "Course" (
     "id" TEXT NOT NULL,
-    "levelId" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION,
+    "imageUrl" TEXT,
     "status" "CourseStatus" NOT NULL,
+    "levelId" TEXT NOT NULL,
+    "skillId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -107,10 +125,22 @@ CREATE TABLE "Course" (
 );
 
 -- CreateTable
+CREATE TABLE "Attachment" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "courseId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Attachment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Lesson" (
     "id" TEXT NOT NULL,
-    "courseId" TEXT NOT NULL,
     "status" "LessonStatus" NOT NULL,
+    "courseId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -130,7 +160,16 @@ CREATE INDEX "User_username_email_idx" ON "User"("username", "email");
 CREATE UNIQUE INDEX "Country_code_key" ON "Country"("code");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Skill_name_key" ON "Skill"("name");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Level_levelCode_key" ON "Level"("levelCode");
+
+-- CreateIndex
+CREATE INDEX "Course_skillId_idx" ON "Course"("skillId");
+
+-- CreateIndex
+CREATE INDEX "Attachment_courseId_idx" ON "Attachment"("courseId");
 
 -- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -149,6 +188,12 @@ ALTER TABLE "Certification" ADD CONSTRAINT "Certification_tutorId_fkey" FOREIGN 
 
 -- AddForeignKey
 ALTER TABLE "Course" ADD CONSTRAINT "Course_levelId_fkey" FOREIGN KEY ("levelId") REFERENCES "Level"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Course" ADD CONSTRAINT "Course_skillId_fkey" FOREIGN KEY ("skillId") REFERENCES "Skill"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attachment" ADD CONSTRAINT "Attachment_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Lesson" ADD CONSTRAINT "Lesson_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ backend/prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const abran = await prisma.user.create({
+    data: {
+      name: 'Abraham',
+      lastName: 'Hernández',
+      email: 'abran@gmail.com',
+      username: 'abranher',
+      password: await hash('abran123', 10),
+      role: 'ADMIN',
+    },
+  });
+
+  console.log({ abran });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
