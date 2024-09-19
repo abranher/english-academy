73b207bb77106e09d2a7f2e3f3030055f65a42cd
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-09-19 17:48:45 -0400

    front: add settings page with funcionality

--- frontend/app/(users)/student/settings/page.tsx
+++ frontend/app/(users)/student/settings/page.tsx
@@ -5,9 +5,9 @@ export default function SettingsProfileStudentPage() {
   return (
     <div className="space-y-6">
       <div>
-        <h3 className="text-lg font-medium">Profile</h3>
+        <h3 className="text-lg font-medium">Perfil</h3>
         <p className="text-sm text-muted-foreground">
-          This is how others will see you on the site.
+          Así te verán los demás en la plataforma.
         </p>
       </div>
       <Separator />
