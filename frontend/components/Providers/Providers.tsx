4a764393539b9ca666a89e9d7325e56f2b404206
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-09-19 03:00:24 -0400

    front: add next-auth@beta functionality

--- frontend/components/Providers/Providers.tsx
+++ frontend/components/Providers/Providers.tsx
@@ -2,11 +2,15 @@
 
 import { SessionProvider } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import queryClient from "@/config/react-query";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider>
