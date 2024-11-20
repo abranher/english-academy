105d24bca470be29b8a2b5aa61577bef5396b70e
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-11-20 05:41:33 -0400

    front: file input image uploading

--- frontend/app/(users)/tutor/courses/[courseId]/_components/ImageForm.tsx
+++ frontend/app/(users)/tutor/courses/[courseId]/_components/ImageForm.tsx
@@ -1,22 +1,27 @@
 "use client";
 
-import NextImage from "next/image";
 import axios from "@/config/axios";
 import { useRouter } from "next/navigation";
 
 import { useCallback, useState } from "react";
-
 import { useDropzone } from "react-dropzone";
+
 import { Button } from "@/components/shadcn/ui/button";
 import { CardContent } from "@/components/shadcn/ui/card";
-import { toast } from "sonner";
 import { Image } from "@nextui-org/react";
-import { ImageIcon, UploadCloud, XIcon } from "lucide-react";
+import { toast } from "sonner";
+import {
+  CheckCircle,
+  ImageIcon,
+  Loader2,
+  UploadCloud,
+  XIcon,
+} from "lucide-react";
 import asset from "@/libs/asset";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -32,7 +37,8 @@ interface ImageFormProps {
 
 export default function ImageForm({ course }: any) {
   const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
-  const [progress, setProgress] = useState(13);
+  const [progress, setProgress] = useState(0);
+  const [uploadStatus, setUploadStatus] = useState("select");
 
   // drop zone
   const onDrop = useCallback((acceptedFiles: any) => {
@@ -48,69 +54,90 @@ export default function ImageForm({ course }: any) {
 
     if (typeof selectedFile === "undefined") return;
 
-    const formData = new FormData();
-    formData.set("image", selectedFile);
-
     try {
-      await axios.post(`/api/courses/${course.courseId}/image`, formData);
+      setUploadStatus("uploading");
 
+      const formData = new FormData();
+      formData.set("image", selectedFile);
+
+      await axios.post(`/api/courses/${course.id}/image`, formData, {
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / (progressEvent.total || 1)
+          );
+
+          setProgress(percentCompleted);
+        },
+      });
+
+      setUploadStatus("done");
       toast.success("Imagen actualizada!");
       setSelectedFile(undefined);
       router.refresh();
     } catch (error) {
+      setProgress(0);
+      setUploadStatus("select");
       toast.error("Something wrong");
     }
   };
 
   const clearFileInput = () => {
     setSelectedFile(undefined);
+    setProgress(0);
+    setUploadStatus("select");
+  };
+
+  const formatSize = (size: number) => {
+    return `${(size / 1024).toFixed(2)} KB o ${(size / 1024 / 1024).toFixed(
+      2
+    )} MB`;
   };
 
   return (
     <>
       <CardContent>
-        <form onSubmit={onSubmit}>
-          <section className="grid grid-cols-1 md:grid-cols-2">
-            {/** File upload */}
-            {course.image ? (
-              <div className="h-[240px] w-[290px] rounded-lg">
-                <Image
-                  src={asset(course.image)}
-                  className="aspect-video"
-                  alt={course.title}
-                />
-              </div>
-            ) : (
-              <div className="grid aspect-video place-items-center rounded-lg bg-zinc-200 dark:bg-zinc-800">
-                <ImageIcon className="h-9 w-9 text-gray-600 aspect-video" />
-              </div>
-            )}
+        <section className="grid grid-cols-1 md:grid-cols-2">
+          {/** File upload */}
+          {course.image ? (
+            <div className="h-[240px] w-[290px] rounded-lg">
+              <Image
+                src={asset(course.image)}
+                className="aspect-video"
+                alt={course.title}
+              />
+            </div>
+          ) : (
+            <div className="grid aspect-video place-items-center rounded-lg bg-zinc-200 dark:bg-zinc-800">
+              <ImageIcon className="h-9 w-9 text-gray-600 aspect-video" />
+            </div>
+          )}
 
-            {/** Description */}
-            <article className="px-5 py-2 flex flex-col gap-3">
-              <h2 className="font-semibold">Imagen del curso</h2>
-              <div className="text-small flex flex-col gap-3">
-                <p>
-                  Es lo primero que ven los estudiantes. Una imagen atractiva
-                  genera curiosidad y ganas de conocer más sobre el curso.
-                </p>
-                <p>
-                  Recomendamos un tamaño de 1024x576 píxeles para una mejor
-                  visualización
-                </p>
-              </div>
+          {/** Description */}
+          <article className="px-5 py-2 flex flex-col gap-3">
+            <h2 className="font-semibold">Imagen del curso</h2>
+            <div className="text-small flex flex-col gap-3">
+              <p>
+                Es lo primero que ven los estudiantes. Una imagen atractiva
+                genera curiosidad y ganas de conocer más sobre el curso.
+              </p>
+              <p>
+                Recomendamos un tamaño de 1024x576 píxeles para una mejor
+                visualización
+              </p>
+            </div>
 
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button className="flex gap-3">
-                    <UploadCloud />
-                    Subir imagen
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                  <DialogHeader>
-                    <DialogTitle>Subir imagen</DialogTitle>
-                  </DialogHeader>
+            <Dialog onOpenChange={clearFileInput}>
+              <DialogTrigger asChild>
+                <Button className="flex gap-3">
+                  <UploadCloud />
+                  Subir imagen
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Subir imagen</DialogTitle>
+                </DialogHeader>
+                <form onSubmit={onSubmit}>
                   <div className="grid gap-4 py-4">
                     {selectedFile ? (
                       <>
@@ -130,101 +157,92 @@ export default function ImageForm({ course }: any) {
                       </>
                     )}
                     {/** upload input */}
-                    <label
-                      {...getRootProps()}
-                      className="p-5 bg-gray-100 text-gray-600 font-semibold text-xs rounded h-32 flex flex-col items-center justify-center cursor-pointer border-3 border-gray-300 border-dashed"
-                    >
-                      <UploadCloud className="w-11 mb-2" />
-                      Subir archivo
-                      <input {...getInputProps()} className="hidden" />
-                      {isDragActive ? (
-                        <p>Suelta los archivos aquí ...</p>
-                      ) : (
-                        <p>
-                          Arrastra y suelta tus archivos aquí o{" "}
-                          <span className="font-medium text-blue-600 hover:underline">
-                            explora
-                          </span>
+                    {(uploadStatus === "select" ||
+                      uploadStatus === "uploading") && (
+                      <label
+                        {...getRootProps()}
+                        className="p-5 bg-gray-100 text-gray-600 font-semibold text-xs rounded h-32 flex flex-col items-center justify-center cursor-pointer border-3 border-gray-300 border-dashed"
+                      >
+                        <UploadCloud className="w-11 mb-2" />
+                        Subir archivo
+                        <input {...getInputProps()} className="hidden" />
+                        {isDragActive ? (
+                          <p>Suelta los archivos aquí ...</p>
+                        ) : (
+                          <p>
+                            Arrastra y suelta tus archivos aquí o{" "}
+                            <span className="font-medium text-blue-600 hover:underline">
+                              explora
+                            </span>
+                          </p>
+                        )}
+                        <p className="text-xs font-medium mt-2">
+                          Se permiten PNG, JPG, WEBP.
                         </p>
-                      )}
-                      <p className="text-xs font-medium mt-2">
-                        Se permiten PNG, JPG, WEBP.
-                      </p>
-                    </label>
-
-                    {selectedFile && (
-                      <>
-                        <section className="grid grid-cols-8 py-3 rounded-lg text-gray-700 bg-zinc-200 dark:bg-zinc-800">
-                          <div className="col-span-1 flex justify-center items-center">
-                            <ImageIcon />
-                          </div>
-                          <div className="text-xs col-span-6 flex flex-col justify-center gap-1">
-                            <p>{selectedFile.name}</p>
-                            <p>
-                              {`${(selectedFile.size / 1024).toFixed(
-                                2
-                              )} KB o ${(
-                                selectedFile.size /
-                                1024 /
-                                1024
-                              ).toFixed(2)} MB`}
-                            </p>
-                            <Progress value={progress} className="h-2" />
-                          </div>
-                          <div className="col-span-1 flex justify-center items-center">
-                            <XIcon
-                              className="cursor-pointer"
-                              onClick={clearFileInput}
-                            />
-                          </div>
-                        </section>
-                        <Button type="submit">
-                          <UploadCloud />
-                        </Button>
-                      </>
+                      </label>
                     )}
+
+                    <>
+                      <section className="grid grid-cols-8 py-3 rounded-lg text-gray-700 bg-zinc-200 dark:bg-zinc-800">
+                        <div className="col-span-1 flex justify-center items-center">
+                          <ImageIcon />
+                        </div>
+                        <div className="text-xs col-span-6 flex flex-col justify-center gap-1">
+                          <p>
+                            {selectedFile
+                              ? selectedFile.name
+                              : "No hay ningún archivo seleccionado"}
+                          </p>
+                          <p>{selectedFile && formatSize(selectedFile.size)}</p>
+                          <Progress value={progress} className="h-2" />
+                        </div>
+                        {uploadStatus === "select" && selectedFile && (
+                          <>
+                            <div className="col-span-1 flex justify-center items-center">
+                              <XIcon
+                                className="cursor-pointer"
+                                onClick={clearFileInput}
+                              />
+                            </div>
+                          </>
+                        )}
+                        {uploadStatus === "uploading" && (
+                          <>
+                            <div className="col-span-1 flex justify-center items-center">
+                              {progress}
+                            </div>
+                          </>
+                        )}
+                        {uploadStatus === "done" && (
+                          <>
+                            <div className="col-span-1 flex justify-center items-center">
+                              <CheckCircle />
+                            </div>
+                          </>
+                        )}
+                      </section>
+                      <Button
+                        type="submit"
+                        disabled={
+                          uploadStatus === "uploading" ||
+                          uploadStatus === "done"
+                        }
+                      >
+                        {uploadStatus === "select" && <UploadCloud />}
+
+                        {uploadStatus === "uploading" && (
+                          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                        )}
+
+                        {uploadStatus === "done" && <CheckCircle />}
+                      </Button>
+                    </>
                   </div>
-                </DialogContent>
-              </Dialog>
-            </article>
-          </section>
-
-          {selectedFile && (
-            <>
-              <article className="flex items-center space-x-6 p-6">
-                {selectedFile.type.startsWith("image/") && (
-                  <Image
-                    as={NextImage}
-                    src={URL.createObjectURL(selectedFile)}
-                    alt="Preview"
-                    width={100}
-                    height={88}
-                    className="mt-2 rounded-md max-w-full"
-                  />
-                )}
-                <div className="min-w-0 relative flex-auto">
-                  <p className="text-sm font-bold">
-                    Nombre del archivo: {selectedFile.name}
-                  </p>
-                  <dl className="mt-2 flex flex-wrap text-sm leading-6 font-medium">
-                    <div>
-                      <dt className="sr-only">Rating</dt>
-                      <dd className="px-1.5 ring-1 ring-slate-200 rounded">
-                        Tamaño del archivo:{" "}
-                      </dd>
-                    </div>
-                  </dl>
-                </div>
-              </article>
-
-              <div className="w-full">
-                <div className="justify-end flex px-4">
-                  <Button>Subir</Button>
-                </div>
-              </div>
-            </>
-          )}
-        </form>
+                </form>
+              </DialogContent>
+            </Dialog>
+          </article>
+        </section>
       </CardContent>
     </>
   );
