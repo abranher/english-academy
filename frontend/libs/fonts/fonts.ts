804e60de09865c34426fbe583a98e012bfa9a3e8
Abraham Hernandez <abrahamycaldera@gmail.com>
2025-03-05 05:31:10 -0400

    front: local fonts added

--- frontend/libs/fonts/fonts.ts
+++ frontend/libs/fonts/fonts.ts
@@ -1,3 +1,26 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-export const fontInter = Inter({ subsets: ["latin"] });
+// export const fontInter = Inter({ subsets: ["latin"] });
+
+export const fontInter = localFont({
+  src: [
+    {
+      path: "./static/Inter_18pt-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "./static/Inter_18pt-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "./static/Inter_18pt-Medium.ttf",
+      weight: "600",
+    },
+    {
+      path: "./static/Inter_18pt-Bold.ttf",
+      weight: "700",
+    },
+  ],
+  display: "swap",
+});
