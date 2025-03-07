84646316359a0be463b3656c3e6c425a7300ba4e
Abraham Hernandez <abrahamycaldera@gmail.com>
2025-03-07 05:40:33 -0400

    back: socket.io package added

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -3,11 +3,12 @@ import { Global, Module } from '@nestjs/common';
 import { NotificationsService } from './providers/notifications.service';
 import { NotificationsController } from './controllers/notifications.controller';
 import { UpdatedTutorStatus } from './class/updated-tutor-status';
+import { NotificationsGateway } from './gateways/notifications.gateway';
 
 @Global()
 @Module({
   controllers: [NotificationsController],
-  providers: [NotificationsService, UpdatedTutorStatus],
+  providers: [NotificationsService, NotificationsGateway, UpdatedTutorStatus],
   exports: [NotificationsService, UpdatedTutorStatus],
 })
 export class NotificationsModule {}
