8cd6cb72532b07e8485dcca9c1b98e9b21fef5df
Abraham Hernandez <136014303+abranher@users.noreply.github.com>
2025-03-17 20:31:45 -0400

    Actualizar Attachment.ts

--- frontend/types/models/Attachment.ts
+++ frontend/types/models/Attachment.ts
@@ -8,3 +8,14 @@ export interface Attachment {
   createdAt: Date;
   updatedAt: Date;
 }
+
+
+export interface Attachment {
+  id: string;
+  title: string;
+  url: string;
+  tutorId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  classAttachmentId?: string; // Nueva propiedad para almacenar el ID de ClassAttachment
+}
