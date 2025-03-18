5a619090f949c13ec205646d2d24a69b88fd298f
Abraham Hernandez <136014303+abranher@users.noreply.github.com>
2025-03-17 20:32:57 -0400

    Actualizar Attachment.ts

--- frontend/types/models/Attachment.ts
+++ frontend/types/models/Attachment.ts
@@ -19,3 +19,15 @@ export interface Attachment {
   updatedAt: Date;
   classAttachmentId?: string; // Nueva propiedad para almacenar el ID de ClassAttachment
 }
+
+
+const attachedAttachments: Attachment[] = lessonClass.attachments
+  .map((classAttachment) => {
+    const attachment = attachments?.find(
+      (a) => a.id === classAttachment.attachmentId
+    );
+    return attachment
+      ? { ...attachment, classAttachmentId: classAttachment.id } // Incluye el ID de ClassAttachment
+      : null;
+  })
+  .filter((attachment): attachment is Attachment => attachment !== null);
