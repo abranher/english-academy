1b8e8c0a003bc5e7318fa8867e81507e2853a90d
Abraham Hernandez <abrahamycaldera@gmail.com>
2025-03-22 04:26:33 -0400

    front: payment profile for admin works stable

--- backend/src/modules/mobile-payments/mobile-payments.module.ts
+++ backend/src/modules/mobile-payments/mobile-payments.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MobilePaymentsService } from './providers/mobile-payments.service';
+
+import { UsersModule } from '../users/users.module';
 import { MobilePaymentsController } from './controllers/mobile-payments.controller';
+import { MobilePaymentsAdminController } from './controllers/mobile-payments.admin.controller';
+import { MobilePaymentsService } from './providers/mobile-payments.service';
+import { MobilePaymentsAdminService } from './providers/mobile-payments.admin.service';
 
 @Module({
-  controllers: [MobilePaymentsController],
-  providers: [MobilePaymentsService],
+  imports: [UsersModule],
+  controllers: [MobilePaymentsController, MobilePaymentsAdminController],
+  providers: [MobilePaymentsService, MobilePaymentsAdminService],
 })
 export class MobilePaymentsModule {}
