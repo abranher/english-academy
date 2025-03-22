04341102fd452e3d7fc0e7eefad8243da41e95b2
Abraham Hernandez <136014303+abranher@users.noreply.github.com>
2025-03-21 21:51:56 -0400

    Actualizar banks.ts

--- backend/prisma/data/banks.ts
+++ backend/prisma/data/banks.ts
@@ -16,7 +16,99 @@ export const banks = [
     name: 'Mercantil Banco, C.A. Banco Universal',
   },
   {
-    code: '0175',
-    name: 'Banco de los Trabajadores del Pueblo',
+    code: '0108',
+    name: 'BBVA Provincial, S.A. Banco Universal',
+  },
+  {
+    code: '0114',
+    name: 'Bancaribe C.A. Banco Universal',
+  },
+  {
+    code: '0115',
+    name: 'Banco Exterior C.A. Banco Universal',
+  },
+  {
+    code: '0128',
+    name: 'Banco Caroní C.A. Banco Universal',
+  },
+  {
+    code: '0134',
+    name: 'Banesco, Banco Universal S.A.C.A.',
+  },
+  {
+    code: '0137',
+    name: 'Banco Sofitasa, Banco Universal',
+  },
+  {
+    code: '0138',
+    name: 'Banco Plaza, Banco Universal',
+  },
+  {
+    code: '0146',
+    name: 'Bangente C.A',
+  },
+  {
+    code: '0151',
+    name: 'BFC Banco Fondo Común C.A. Banco Universal',
+  },
+  {
+    code: '0156',
+    name: '100% Banco, Banco Universal C.A.',
+  },
+  {
+    code: '0157',
+    name: 'DelSur Banco Universal C.A.',
+  },
+  {
+    code: '0163',
+    name: 'Banco del Tesoro, C.A. Banco Universal',
+  },
+  {
+    code: '0166',
+    name: 'Banco Agrícola de Venezuela, C.A. Banco Universal',
+  },
+  {
+    code: '0168',
+    name: 'Bancrecer, S.A. Banco Microfinanciero',
+  },
+  {
+    code: '0169',
+    name: 'R4, Banco Microfinanciero C.A.',
+  },
+  {
+    code: '0171',
+    name: 'Banco Activo, Banco Universal',
+  },
+  {
+    code: '0172',
+    name: 'Bancamiga, Banco Universal C.A.',
+  },
+  {
+    code: '0173',
+    name: 'Banco Internacional de Desarrollo, C.A. Banco Universal',
+  },
+  {
+    code: '0174',
+    name: 'Banplus Banco Universal, C.A',
+  },
+  {
+    code: '0007',
+    name: 'Banco Digital de Los Trabajadores',
+  },
+  {
+    code: '0177',
+    name: 'Banco de la Fuerza Armada Nacional Bolivariana, B.U.',
+  },
+  {
+    code: '0178',
+    name: 'N58 Banco Digital, S.A.',
+  },
+  {
+    code: '0191',
+    name: 'Banco Nacional de Crédito, C.A. Banco Universal',
+  },
+  {
+    code: '0601',
+    name: 'Instituto Municipal de Crédito Popular',
   },
 ];
