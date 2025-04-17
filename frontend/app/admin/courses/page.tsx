784c449157f754bfe5deb505d9e78baa2870f6a7
Abraham Hernandez <abrahamycaldera@gmail.com>
2025-04-17 17:32:50 -0400

    front: admin pages updated

--- frontend/app/admin/courses/page.tsx
+++ frontend/app/admin/courses/page.tsx
@@ -50,50 +50,6 @@ export default async function CoursesPage() {
         <CardTitle>Cursos</CardTitle>
         <CardDescription>Listado de cursos</CardDescription>
       </section>
-
-      <Separator />
-
-      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Ingresos totales
-            </CardTitle>
-            <DollarSign className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-            <p className="text-xs text-muted-foreground">
-              +20,1% respecto al mes pasado
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Suscripciones</CardTitle>
-            <Users className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+2350</div>
-            <p className="text-xs text-muted-foreground">
-              +180,1% respecto al mes pasado
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Ventas</CardTitle>
-            <CreditCard className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
-            <p className="text-xs text-muted-foreground">
-              +19% respecto al mes pasado
-            </p>
-          </CardContent>
-        </Card>
-      </div>
-
       <Separator />
 
       <CoursesList />
